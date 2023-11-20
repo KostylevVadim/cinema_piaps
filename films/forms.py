db64f69d29eeaae593cff4ddc9f60c0e12058710
Антон Глушко <antonglushsko2002a@mail.ru>
2023-11-20 14:00:40 +0300

    Fixes

--- films/forms.py
+++ films/forms.py
@@ -1,14 +1,14 @@
 from django import forms
-from django.contrib.auth import authenticate, get_user_model
-from django.contrib.auth.forms import UserCreationForm
 
 from database.models import films
 
-class UploadFilm(forms.ModelForm):
-    title = forms.CharField(primary_key=True, max_length=128, unique= True)
-    path = forms.FileField(upload_to='cinema')
-    rating = forms.DecimalField(max_digits=6, decimal_places= 2)
+class UploadFilm(forms.Forms):
+    title = forms.CharField(max_length=128)
+    description = forms.CharField(max_length=500)
+    genre = forms.CharField(max_length=100)
+    data = forms.FileField()
+
 
     class Meta:
         model = films
-        fields = ('title', 'rating', 'path')
+        fields = ('title', 'description', 'genre', 'data')
